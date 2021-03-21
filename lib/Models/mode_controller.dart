3e1d192b3b0f8e24612b03e9b991a79fdb89b680
BringtBlack <guerramatias>
2021-03-21 02:43:46 -0300

    Mode controller

--- lib/Models/mode_controller.dart
+++ lib/Models/mode_controller.dart
@@ -1,10 +1,22 @@
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/foundation.dart';
 
 class ModeController extends ChangeNotifier{
   bool isOnline = true;
 
-  void changeMode(bool value){
-    isOnline = value;
+  Future<void> changeMode({bool connectivityBoolean}) async{
+    print("entro a change mode");
+    if(connectivityBoolean == true) {
+      var connectivityResult = await Connectivity().checkConnectivity();
+      if(connectivityResult != ConnectivityResult.none){
+        isOnline = connectivityBoolean;
+      }else{
+        print("No connection, cant turn online mode on");
+      }
+    }else{
+      print("set false");
+      isOnline = connectivityBoolean;
+    }
     notifyListeners();
   }
 }
