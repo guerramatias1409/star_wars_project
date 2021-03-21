6ab1ab25404f30b7885e0749737cac8b75103e08
BringtBlack <guerramatias>
2021-03-21 02:43:30 -0300

    service - use futures again / add getContext()

--- lib/service.dart
+++ lib/service.dart
@@ -1,14 +1,21 @@
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'package:star_wars_project/Models/mode_controller.dart';
 import 'package:star_wars_project/Models/planet.dart';
 import 'package:star_wars_project/Models/starship.dart';
 import 'package:star_wars_project/Models/vehicle.dart';
 import 'package:star_wars_project/Models/character.dart';
 import 'dart:convert';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:star_wars_project/main.dart';
 
+BuildContext getContext() {
+  return navigatorKey.currentContext;
+}
 
 Future<List<Character>> getAllCharacters() async {
-    print("getAllCharacters");
+  /*print("getAllCharacters");
     List<Character> characters = [];
     String jsonPage = await rootBundle.loadString('Assets/people1.json');
     final jsonResponse = json.decode(jsonPage);
@@ -17,77 +24,88 @@ Future<List<Character>> getAllCharacters() async {
       characters.add(Character.fromJson(element));
     });
     print("CHARACTERS LENGHT: ${characters.length}");
-    return characters;
+    return characters;*/
 
-    /*String charactersUrl = 'https://swapi.dev/api/people/';
-    List<Character> characters = [];
-    var response = await http.get(Uri.parse(charactersUrl));
-    var jsonData = json.decode(response.body);
-    print("RESPONSE ${response.statusCode}");
-    print("RESPONSE BODY ${response.body}");
+  String charactersUrl = 'https://swapi.dev/api/people/';
+  List<Character> characters = [];
+
+  //TODO esta funcion solo toma los primeros 10 resultados
+  var response = await http.get(Uri.parse(charactersUrl));
+  var jsonData = json.decode(response.body);
+  var list = jsonData['results'] as List;
+  list.forEach((element) async {
+    characters.add(Character.fromJson(element));
+  });
+
+  //TODO La funcion de aca abajo es la que corresponde
+  /*int index = 0;
+  var jsonData;
+  do {
+    var response = await http.get(Uri.parse(index == 0
+        ? charactersUrl
+        : jsonData['next'].replaceAll('http:', 'https:')));
+    jsonData = json.decode(response.body);
     var list = jsonData['results'] as List;
     list.forEach((element) async {
       characters.add(Character.fromJson(element));
     });
-    while(jsonData['next'] != "" && jsonData['next'] != null){
-      String next = jsonData['next'];
-      response = await http.get(Uri.parse(next.replaceAll('http:', 'https:')));
-      jsonData = json.decode(response.body);
-      list = jsonData['results'] as List;
-      list.forEach((element) async {
-        characters.add(Character.fromJson(element));
-      });
-    }
-    print("CHARACTERS LENGHT: ${characters.length}");
-    return characters;*/
+    index++;
+  } while (jsonData['next'] != "" && jsonData['next'] != null);*/
+
+  print("CHARACTERS LENGHT: ${characters.length}");
+
+  return characters;
 }
 
-String getPlanet(String planetEndpoint) {
+/*String getPlanet(String planetEndpoint) {
   return planetEndpoint;
+}*/
+
+Future<Planet> getPlanet(String planetEndpoint) async {
+  var response =
+      await http.get(Uri.parse(planetEndpoint.replaceAll('http:', 'https:')));
+  return Planet.fromJson(json.decode(response.body));
 }
 
-/*Future<Planet> getPlanet(String planetEndpoint) async{
-    var response = await http.get(Uri.parse(planetEndpoint.replaceAll('http:', 'https:')));
-    return Planet.fromJson(json.decode(response.body));
-  }*/
-
-List<String> getCharacterVehicles(dynamic vehiclesList) {
+/*List<String> getCharacterVehicles(dynamic vehiclesList) {
   List<String> vehicles = [];
   var list = vehiclesList as List;
   list.forEach((element) async {
     vehicles.add(element.toString());
   });
   return vehicles;
+}*/
+
+Future<List<Vehicle>> getCharacterVehicles(dynamic vehiclesList) async {
+  List<Vehicle> vehicles = [];
+  var list = vehiclesList as List;
+  list.forEach((element) async {
+    var response =
+        await http.get(Uri.parse(element.replaceAll("http", "https")));
+    vehicles.add(Vehicle.fromJson(json.decode(response.body)));
+  });
+  return vehicles;
 }
 
-/*Future<List<Vehicle>> getCharacterVehicles(dynamic vehiclesList) async{
-    List<Vehicle> vehicles = [];
-    var list = vehiclesList as List;
-    list.forEach((element) async{
-      var response = await http.get(Uri.parse(element.replaceAll("http", "https")));
-      vehicles.add(Vehicle.fromJson(json.decode(response.body)));
-    });
-    return vehicles;
-  }*/
-
-List<String> getCharacterStarships(dynamic starshipsList) {
+/*List<String> getCharacterStarships(dynamic starshipsList) {
   List<String> starships = [];
   var list = starshipsList as List;
   list.forEach((element) async {
     starships.add(element);
   });
   return starships;
-}
+}*/
 
-/*Future<List<Starship>> getCharacterStarships(dynamic starshipsList) async{
-    List<Starship> starships = [];
-    var list = starshipsList as List;
-    list.forEach((element) async{
-      var response = await http.get(Uri.parse(element.replaceAll("http", "https")));
-      starships.add(Starship.fromJson(json.decode(response.body)));
-    });
-    return starships;
-  }*/
+Future<List<Starship>> getCharacterStarships(dynamic starshipsList) async {
+  List<Starship> starships = [];
+  var list = starshipsList as List;
+  list.forEach((element) async {
+    var response =
+        await http.get(Uri.parse(element.replaceAll("http", "https")));
+    starships.add(Starship.fromJson(json.decode(response.body)));
+  });
+  return starships;
+}
 
 Future<void> sendPost(Character character) async {
   print("send post init");
