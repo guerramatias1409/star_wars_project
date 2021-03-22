41fa2749a9fc42d38285070c89abba3f38a7e3be
BringtBlack <guerramatias>
2021-03-22 20:15:17 -0300

    First Screen - add drawer

--- lib/Screens/first_screen.dart
+++ lib/Screens/first_screen.dart
@@ -27,9 +27,9 @@ class _FirstScreenState extends State<FirstScreen>
   @override
   void initState() {
     print("first screen init state");
-    animationController = AnimationController(
-        vsync: this, duration: Duration(seconds: 3));
-    Timer(Duration(seconds: 1),(){
+    animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 3));
+    Timer(Duration(seconds: 1), () {
       animationController.forward();
     });
     checkInitialConnection();
@@ -49,103 +49,135 @@ class _FirstScreenState extends State<FirstScreen>
                   image: DecorationImage(
                       fit: BoxFit.fitHeight, image: background)),
             ),
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(
-                      top: 30, left: 16, right: 16, bottom: 10),
-                  child: FadeTransition(
-                      opacity: animationController
-                          .drive(CurveTween(curve: Curves.easeOut)),
-                      child: Column(
-                        children: [
-                          Container(
-                            height: 150,
-                            decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    fit: BoxFit.cover, image: logo)),
-                          ),
-                          BorderedText(
-                            strokeWidth: 4,
-                            strokeColor: Color(0xFFFFE444),
-                            child: Text(
-                              "INVASION",
-                              style: Theme.of(context).textTheme.subtitle1,
-                            ),
-                          )
-                        ],
-                      )),
-                ),
-                SizedBox(height: 10),
-                connectivityController.isOnline == false && _future == null
-                    ? Center(
-                        child: Text(
-                        "You need connection to get data",
-                        style: TextStyle(color: Colors.white),
-                      ))
-                    : Flexible(
-                        child: FutureBuilder(
-                          future: _future,
-                          builder: (context, snapshot) {
-                            if (snapshot.connectionState ==
-                                ConnectionState.done) {
-                              return GridView.count(
-                                padding: EdgeInsets.symmetric(horizontal: 8),
-                                crossAxisCount: 2,
-                                children: _characterList(snapshot.data),
-                              );
-                            } else {
-                              return LoadingWidget();
-                            }
-                          },
-                        ),
-                      )
-              ],
-            )
-
-/*          Scaffold(
+            Scaffold(
               backgroundColor: Colors.transparent,
+              drawer: Theme(
+                data: Theme.of(context).copyWith(
+                  canvasColor: Colors.white.withOpacity(
+                      0.9), //This will change the drawer background to blue.
+                  //other styles
+                ),
+                child: Drawer(
+                  child: ListView(
+                    children: [
+                      //DrawerHeader(child: child),
+                      Container(
+                          padding:
+                              const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
+                          margin: const EdgeInsets.only(bottom: 8.0),
+                          child: Text(
+                            "Manage App Modes",
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.bold),
+                          )),
+                      Divider(height: 5, color: Colors.black),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Consumer(builder: (BuildContext context,
+                          ModeController connectivityController, Widget child) {
+                        return Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 16),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(connectivityController.isOnline
+                                      ? "Online Mode"
+                                      : "Offline Mode",
+                                  style: TextStyle(
+                                    fontSize: 22
+                                  )),
+                                  Switch(
+                                      activeColor: Colors.black,
+                                      inactiveThumbColor: Colors.black.withOpacity(0.4),
+                                      value: connectivityController.isOnline,
+                                      onChanged: (value) async {
+                                        await connectivityController.changeMode(
+                                            connectivityBoolean: value);
+                                        if (connectivityController.isOnline ==
+                                            true) {
+                                          refreshList();
+                                        }
+                                      })
+                                ],
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 10),
+                                child: Text("(Offline Mode won't allow you to reload data or send reports. To do this, please switch to Online Mode)",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        fontSize: 13
+                                    )),
+                              ),
+                            ],
+                          ),
+                        );
+                      }),
+                    ],
+                  ),
+                ),
+              ),
               appBar: AppBar(
-                title: Text("Star Wars Project"),
-                actions: [
-                  Consumer(builder: (BuildContext context,
-                      ModeController connectivityController, Widget child) {
-                    return Switch(
-                        value: connectivityController.isOnline,
-                        onChanged: (value) async{
-                          await connectivityController.changeMode(
-                              connectivityBoolean: value);
-                          if(connectivityController.isOnline == true){
-                            refreshList();
-                          }
-                        });
-                  })
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                iconTheme: IconThemeData(color: Colors.white),
+              ),
+              body: Column(
+                children: [
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 16, right: 16, bottom: 10),
+                    child: FadeTransition(
+                        opacity: animationController
+                            .drive(CurveTween(curve: Curves.easeOut)),
+                        child: Column(
+                          children: [
+                            Container(
+                              height: 150,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      fit: BoxFit.cover, image: logo)),
+                            ),
+                            BorderedText(
+                              strokeWidth: 4,
+                              strokeColor: Color(0xFFFFE444),
+                              child: Text(
+                                "INVASION",
+                                style: Theme.of(context).textTheme.subtitle1,
+                              ),
+                            )
+                          ],
+                        )),
+                  ),
+                  SizedBox(height: 10),
+                  connectivityController.isOnline == false && _future == null
+                      ? Center(
+                          child: Text(
+                          "You need connection to get data",
+                          style: TextStyle(color: Colors.white),
+                        ))
+                      : Flexible(
+                          child: FutureBuilder(
+                            future: _future,
+                            builder: (context, snapshot) {
+                              if (snapshot.connectionState ==
+                                  ConnectionState.done) {
+                                return GridView.count(
+                                  padding: EdgeInsets.symmetric(horizontal: 8),
+                                  crossAxisCount: 2,
+                                  children: _characterList(snapshot.data),
+                                );
+                              } else {
+                                return LoadingWidget();
+                              }
+                            },
+                          ),
+                        ),
                 ],
               ),
-              body: connectivityController.isOnline == false && _future == null
-                  ? Center(child: Text("You need connection to get data"))
-                  : FutureBuilder(
-                      future: _future,
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState == ConnectionState.done) {
-                          return ListView(
-                            children: _characterList(snapshot.data),
-                          );
-                        } else {
-                          return Center(child: CircularProgressIndicator());
-                        }
-                      },
-                    ),
-              floatingActionButton: connectivityController.isOnline
-                  ? FloatingActionButton(
-                      onPressed: () {
-                        rebuildList();
-                      },
-                      child: Icon(Icons.refresh),
-                    )
-                  : Container(),
-            )*/
-            ,
+            )
           ],
         ),
       );
