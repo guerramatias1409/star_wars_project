f1194d73a57b885874ba7d590ad02b74584454c4
BringtBlack <guerramatias>
2021-03-23 02:11:04 -0300

    Service - delete unused functions

--- lib/service.dart
+++ lib/service.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
-import 'package:provider/provider.dart';
-import 'package:star_wars_project/Models/mode_controller.dart';
 import 'package:star_wars_project/Models/planet.dart';
 import 'package:star_wars_project/Models/starship.dart';
 import 'package:star_wars_project/Models/vehicle.dart';
 import 'package:star_wars_project/Models/character.dart';
 import 'dart:convert';
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:star_wars_project/main.dart';
 
 BuildContext getContext() {
@@ -15,30 +12,10 @@ BuildContext getContext() {
 }
 
 Future<List<Character>> getAllCharacters() async {
-  /*print("getAllCharacters");
-    List<Character> characters = [];
-    String jsonPage = await rootBundle.loadString('Assets/people1.json');
-    final jsonResponse = json.decode(jsonPage);
-    var list = jsonResponse['results'] as List;
-    list.forEach((element) async {
-      characters.add(Character.fromJson(element));
-    });
-    print("CHARACTERS LENGHT: ${characters.length}");
-    return characters;*/
-
   String charactersUrl = 'https://swapi.dev/api/people/';
   List<Character> characters = [];
 
-  //TODO esta funcion solo toma los primeros 10 resultados
-  var response = await http.get(Uri.parse(charactersUrl));
-  var jsonData = json.decode(response.body);
-  var list = jsonData['results'] as List;
-  list.forEach((element) async {
-    characters.add(Character.fromJson(element));
-  });
-
-  //TODO La funcion de aca abajo es la que corresponde
-  /*int index = 0;
+  int index = 0;
   var jsonData;
   do {
     var response = await http.get(Uri.parse(index == 0
@@ -50,31 +27,18 @@ Future<List<Character>> getAllCharacters() async {
       characters.add(Character.fromJson(element));
     });
     index++;
-  } while (jsonData['next'] != "" && jsonData['next'] != null);*/
+  } while (jsonData['next'] != "" && jsonData['next'] != null);
 
   print("CHARACTERS LENGHT: ${characters.length}");
 
   return characters;
 }
 
-/*String getPlanet(String planetEndpoint) {
-  return planetEndpoint;
-}*/
-
 Future<Planet> getPlanet(String planetEndpoint) async {
   var response =
       await http.get(Uri.parse(planetEndpoint.replaceAll('http:', 'https:')));
   return Planet.fromJson(json.decode(response.body));
 }
-
-/*List<String> getCharacterVehicles(dynamic vehiclesList) {
-  List<String> vehicles = [];
-  var list = vehiclesList as List;
-  list.forEach((element) async {
-    vehicles.add(element.toString());
-  });
-  return vehicles;
-}*/
 
 Future<List<Vehicle>> getCharacterVehicles(dynamic vehiclesList) async {
   List<Vehicle> vehicles = [];
@@ -86,15 +50,6 @@ Future<List<Vehicle>> getCharacterVehicles(dynamic vehiclesList) async {
   });
   return vehicles;
 }
-
-/*List<String> getCharacterStarships(dynamic starshipsList) {
-  List<String> starships = [];
-  var list = starshipsList as List;
-  list.forEach((element) async {
-    starships.add(element);
-  });
-  return starships;
-}*/
 
 Future<List<Starship>> getCharacterStarships(dynamic starshipsList) async {
   List<Starship> starships = [];
@@ -108,7 +63,6 @@ Future<List<Starship>> getCharacterStarships(dynamic starshipsList) async {
 }
 
 Future<int> sendPost(Character character) async {
-  print("send post init");
   final response =
       await http.post(Uri.parse('https://jsonplaceholder.typicode.com/posts/'),
           headers: <String, String>{
@@ -121,7 +75,6 @@ Future<int> sendPost(Character character) async {
           }));
 
   if (response.statusCode == 201) {
-    print("POST SENT");
     print("POST: ${response.body}");
     return 1;
   } else {
