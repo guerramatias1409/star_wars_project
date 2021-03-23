68b0fabd3a8b2d732cd40fd06af5974d8dec58c5
BringtBlack <guerramatias>
2021-03-23 00:29:59 -0300

    ModeController - delete prints

--- lib/Models/mode_controller.dart
+++ lib/Models/mode_controller.dart
@@ -5,7 +5,6 @@ class ModeController extends ChangeNotifier{
   bool isOnline = true;
 
   Future<int> changeMode({bool connectivityBoolean}) async{
-    print("entro a change mode");
     if(connectivityBoolean == true) {
       var connectivityResult = await Connectivity().checkConnectivity();
       if(connectivityResult != ConnectivityResult.none){
@@ -13,11 +12,9 @@ class ModeController extends ChangeNotifier{
         notifyListeners();
         return 1;
       }else{
-        print("No connection, cant turn online mode on");
         return 0;
       }
     }else{
-      print("set false");
       isOnline = connectivityBoolean;
       notifyListeners();
       return 1;
